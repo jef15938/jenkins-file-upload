13e8929212c416decb9855514f5f79cc38e054cc
pongpong <neux.jefferypeng@gmail.com>
2020-03-11 18:35:20 +0800

    add non-cps

--- vars/fileUpload.groovy
+++ vars/fileUpload.groovy
@@ -6,6 +6,7 @@ import org.apache.http.entity.mime.*
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.Method.POST
 
+@NonCPS
 def call(String url, File file, String targetPath){
   println "doPost: $url body: ${file.name}"
   def result 
