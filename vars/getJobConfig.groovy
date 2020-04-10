3633e23445b95396c1d3e546a5bdbada457cbe08
pongpong <jef15938@gmail.com>
2020-04-10 18:58:53 +0800

    update getJobConfig

--- vars/getJobConfig.groovy
+++ vars/getJobConfig.groovy
@@ -1,21 +1,22 @@
+import groovy.json.JsonOutput;
+
 @NonCPS
 def call(){
-//   println "doPost: $url body: ${file.name}"
-    println 'in config';
-    def builder = new groovy.json.JsonBuilder()
-    def configJson = builder.config {
-        jobs: [
+    def builder = new groovy.json.JsonBuilder();
+    def configJson = """
         {
-            jobName: 'SND_Frontend_Build_App',
-            nodeName: 'master'
-        },
-        {
-            jobName: 'SND_Frontend_Deliver',
-            nodeName: 'SND_Jenkins'
-        }]
-    };
-    
-    println configJson;
-   
-    configJson;
+            "jobs": 
+            [   
+                {"jobName": "SND_Frontend_DEV_To_Freeze", "nodeName": "SND_Jenkins"}, 
+                {"jobName": "SND_Frontend_Build_App", "nodeName": "master"},
+                {"jobName": "SND_Frontend_Deliver", "nodeName": "SND_Jenkins"},
+                {"jobName": "SND_Frontend_Downgrade", "nodeName": "SND_Jenkins"},
+
+                {"jobName": "SND_Frontend_Build_Web", "nodeName": "SND_Jenkins"},
+                {"jobName": "SND_Frontend_E2E_Test", "nodeName": "SND_Jenkins"}
+            ]
+        }
+    """;
+    def parser = new groovy.json.JsonSlurper()
+    parser.parseText(configJson)
 }
